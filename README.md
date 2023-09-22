80194af940e663561e35cc01228209640e91b9c2
Edwin Rietmeijer <e.rietmeijer@gmail.com>
2023-09-22 16:45:08 +0200

    Made CardsComparar more human readable

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # nl.edwinrietmeijer.setgame.SetGame
 
 An implementation of the popular pattern finding game Set.
+
+## Generating a Linux Java executable
+
+Run mvn package
+
+$ mvn package
+
+Create a file with the java jar hashbang.
+
+$ echo '#!/usr/bin/java -jar' > setgame
+
+We have written the hashbang as a string to the new file setgame.
+
+Write the jar to the file.
+
+$ cat target/setgame-1.jar >> setgame
+
+The >> appends the jar to the receiving file.
+
+This will create a file that has the bash hashbang and the jar appended to it. Next set setgame to executable and try to run the program.
+
+$ chmod +x setgame
+$ ./setgame
--- src/main/java/nl/edwinrietmeijer/setgame/CardsComparer.java
+++ src/main/java/nl/edwinrietmeijer/setgame/CardsComparer.java
@@ -4,24 +4,34 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CardsComparer {
+    // Number of card pattern dimensions
+    private final int DIMS_NO = 4;
+    // Number of cards to compare
+    private final int CARDS_NO = 3;
+
     public boolean isSet(List<Card> cardList) {
 
-        Integer[][] cards = new Integer[3][4];
-        cards[0] = cardList.get(0).getValues();
-        cards[1] = cardList.get(1).getValues();
-        cards[2] = cardList.get(2).getValues();
+        Integer[][] cards = new Integer[CARDS_NO][DIMS_NO];
+
+        for (int i = 0; i < CARDS_NO; i++) { 
+            cards[i] = cardList.get(i).getValues();
+        }
 
 
-        Integer[][] valueCounts = new Integer[4][3];
-        for (int i = 0; i < 4; i++) {
+        Integer[][] valueCounts = new Integer[DIMS_NO][CARDS_NO];
+
+        for (int i = 0; i < DIMS_NO; i++) {
             Arrays.fill(valueCounts[i], 0);
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 3; k++) {
+
+            for (int j = 0; j < CARDS_NO; j++) {
+                for (int k = 0; k < CARDS_NO; k++) {
                     valueCounts[i][j] += (cards[k][i] == j ? 1 : 0);
                 }
             }
         }
+
         boolean isSet = true;
+
         for (Integer[] valueCount : valueCounts) {
             List<Integer> valueCountList = Arrays.asList(valueCount);
             if ((valueCountList.contains(1) && valueCountList.contains(2)) && !valueCountList.contains(3)) {
